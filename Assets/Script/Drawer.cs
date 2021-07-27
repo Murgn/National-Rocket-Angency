9537fe9c8c61b59abb785cd3a9f4d9ad7d6b9853
Murgn <68158297+Murgn@users.noreply.github.com>
2021-07-27 19:35:43 +0100

    I added a drawer

---
+++ Assets/Script/Drawer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Murgn;
+
+namespace Murgn
+{
+    public class Drawer : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }   
+}
